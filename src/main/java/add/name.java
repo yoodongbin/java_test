bcf9eedcf87156e7ec99601b94220cdaf78f9934
goodjwon <firstman9@protonmail.com>
2022-01-07 10:07:47 +0900

    setting 초기화

--- src/main/java/add/name.java
+++ src/main/java/add/name.java
@@ -12,24 +12,24 @@ public class name {
 		String names = "my name is ";
 		return names + name;
 	}
-	
+
 	public static String test_age(int age) {
 		String ages = "my age is ";
 		return ages + age;
 	}
-	
+
 	public static void main(String[] args) {
-		System.out.println(test_name("´õÄÉÀÌ") + "\n" + test_age(20));
-		
-		// Á¤·Ä ±â´É
-		// ¿À¸§Â÷¼ø Á¤·Ä
+		System.out.println(test_name("ë”ì¼€ì´") + "\n" + test_age(20));
+
+		// ì •ë ¬ ê¸°ëŠ¥
+		// ì˜¤ë¦„ì°¨ìˆœ ì •ë ¬
 		List<Integer> notSortedList = Arrays.asList(3,5,2,4,1,7);
 		Stream<Integer> streamAsc = notSortedList.stream().sorted();
-		// ³»¸²Â÷¼ø Á¤·Ä
+		// ë‚´ë¦¼ì°¨ìˆœ ì •ë ¬
 		Stream<Integer> streamDesc = notSortedList.stream().sorted(Comparator.reverseOrder());
 		streamAsc.forEach(System.out::println);
 		streamDesc.forEach(System.out::println);
 	}
 
-	
+
 }
