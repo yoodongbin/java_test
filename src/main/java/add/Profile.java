ff8eae89bb8d1e6836fd881873831e81b451917e
yoodongbin <ehdqls0412@naver.com>
2022-01-07 16:30:28 +0900

    220107_push

--- src/main/java/add/Profile.java
+++ src/main/java/add/Profile.java
@@ -1,10 +1,30 @@
 package add;
 
 public class Profile {
+	String name;
+	int age;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void setAge(int val) {
+		age = val;
 	}
 
+	public void setName(String str) {
+		name = str;
+	}
+
+	public void printName() {
+		System.out.println("My name is "+name);
+	}
+
+	public void printAge() {
+		System.out.println("My age is "+age);
+	}
+
+	public static void main(String[] args) {
+		Profile profile = new Profile();
+		profile.setAge(20);
+		profile.setName("min");
+		profile.printName();
+		profile.printAge();
+	}
 }
